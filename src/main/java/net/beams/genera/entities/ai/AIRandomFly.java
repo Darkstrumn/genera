8a48a1147bfd41cb495597c210743445f97f559f
Ben Sutter <benjaminsutter@aurora-schools.org>
2017-03-29 19:56:48 -0400

    Tiny Faerie Entity added. Nightshades summon them from the otherworld every now and then.
    Signed-off-by: Ben Sutter <benjaminsutter@aurora-schools.org>

---
+++ src/main/java/net/beams/genera/entities/ai/AIRandomFly.java
@@ -0,0 +1,59 @@
+package net.beams.genera.entities.ai;
+
+import net.minecraft.entity.EntityFlying;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityMoveHelper;
+
+import java.util.Random;
+
+public class AIRandomFly extends EntityAIBase
+{
+    private final EntityFlying parentEntity;
+
+    public AIRandomFly(EntityFlying entity)
+    {
+        this.parentEntity = entity;
+        this.setMutexBits(1);
+    }
+
+    /**
+     * Returns whether the EntityAIBase should begin execution.
+     */
+    public boolean shouldExecute()
+    {
+        EntityMoveHelper entitymovehelper = this.parentEntity.getMoveHelper();
+
+        if (!entitymovehelper.isUpdating())
+        {
+            return true;
+        }
+        else
+        {
+            double d0 = entitymovehelper.getX() - this.parentEntity.posX;
+            double d1 = entitymovehelper.getY() - this.parentEntity.posY;
+            double d2 = entitymovehelper.getZ() - this.parentEntity.posZ;
+            double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+            return d3 < 1.0D || d3 > 3600.0D;
+        }
+    }
+
+    /**
+     * Returns whether an in-progress EntityAIBase should continue executing
+     */
+    public boolean continueExecuting()
+    {
+        return false;
+    }
+
+    /**
+     * Execute a one shot task or start executing a continuous task
+     */
+    public void startExecuting()
+    {
+        Random random = this.parentEntity.getRNG();
+        double d0 = this.parentEntity.posX + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+        double d1 = this.parentEntity.posY + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+        double d2 = this.parentEntity.posZ + (double)((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+        this.parentEntity.getMoveHelper().setMoveTo(d0, d1, d2, 1.0D);
+    }
+}
