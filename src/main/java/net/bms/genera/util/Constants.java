f99da8fe107f196e8f1f855712df1a367b974c03
Ben Sutter <benjaminsutter@aurora-schools.org>
2018-02-18 00:57:56 -0500

    Update update.json and version numbers.

--- src/main/java/net/bms/genera/util/Constants.java
+++ src/main/java/net/bms/genera/util/Constants.java
@@ -5,6 +5,6 @@ package net.bms.genera.util;
  */
 public class Constants {
     public static final String MODID = "genera";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.5";
     public static final String NAME = "Genera";
 }
